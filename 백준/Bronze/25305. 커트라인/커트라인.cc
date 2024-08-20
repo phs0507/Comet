31d0cef9ca7c27ba3f69d4aea9091d6e70cea138
phs0507 <tjd278@gmail.com>
2024-08-20 13:06:08 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.cc
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    
+    int arr[1000];
+    int n, k, temp;
+
+	scanf("%d %d", &n, &k);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+    
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (arr[i] < arr[j]) {
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+		if (i == k - 1) break;
+	}
+	printf("%d", arr[k - 1]);
+    
+	return 0;
+}
