0958230ec9b880e4d1853d00165a534954fac817
phs0507 <tjd278@gmail.com>
2024-08-24 22:13:00 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cc
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	char n[10];
+	char tmp;
+
+	scanf("%s", n);
+
+	for (int i = 0; i < strlen(n) - 1; i++) {
+		for (int j = 0; j < strlen(n); j++) {
+			if (n[j] < n[j + 1]) {
+				tmp = n[j];
+				n[j] = n[j + 1];
+				n[j + 1] = tmp;
+			}
+		}
+	}
+
+	printf("%s", n);
+
+	return 0;
+}
